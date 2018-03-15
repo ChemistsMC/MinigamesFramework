618465c4121864692253f3ebb85c6ac73b4316fe
Evan Maddock <maddock.evan@vivaldi.net>
2018-03-15 19:35:53 -0400

    Initial Commit
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

---
+++ src/main/java/com/chemistsmc/minigamesframework/MinigamesFramework.java
@@ -0,0 +1,16 @@
+package com.chemistsmc.minigamesframework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinigamesFramework extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		//
+	}
+
+	@Override
+	public void onDisable() {
+		//
+	}
+}
