1598e3fad12b72b5a55772534a12195a3226b497
Evan Maddock <maddock.evan@vivaldi.net>
2018-03-16 17:18:49 -0400

    Games need states
    Signed-off-by: Evan Maddock <maddock.evan@vivaldi.net>

---
+++ src/main/java/com/chemistsmc/minigamesframework/GameState.java
@@ -0,0 +1,36 @@
+package com.chemistsmc.minigamesframework;
+
+/**
+ * Enumerates the various states that a game can be in.
+ */
+public enum GameState
+{
+
+    /**
+     * Game is waiting for first player to join.
+     */
+    IDLE,
+
+    /**
+     * Game is waiting for all players to join.
+     * If anything needs to be set up, e.g. chests,
+     * this happens in this GameState as well.
+     */
+    PRE_GAME,
+
+    /**
+     * The game is running.
+     */
+    RUNNING,
+
+    /**
+     * The game is over, and the arena needs to be cleaned
+     * up.
+     */
+    POST_GAME,
+
+    /**
+     * The game cannot be used.
+     */
+    DISABLED
+}
